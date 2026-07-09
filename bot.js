db5dfcc4692f964fbcbf91d08bc9d12bf7aa05da
Froo58 <froo5806@gmail.com>
2026-07-09 08:24:27 +0000

    Bot working

--- bot.js
+++ bot.js
@@ -2,28 +2,42 @@ import { Sphere } from '@unicitylabs/sphere-sdk';
 import { createNodeProviders } from '@unicitylabs/sphere-sdk/impl/nodejs';
 
 async function main() {
-  console.log('Sphere testnet tip bot baslatiliyor...');
+  console.log('Sphere testnet2 tip bot baslatiliyor...');
+
+  const providers = createNodeProviders({
+    network: 'testnet',
+    dataDir: './wallet-data',
+    oracle: {
+      apiKey: 'sk_ddc3cfcc001e4a28ac3fad7407f99590',
+    },
+  });
 
   const { sphere, created, generatedMnemonic } = await Sphere.init({
-    ...createNodeProviders({ network: 'testnet' }),
-    autoGenerate: true,
+    ...providers,
+    network: "testnet", autoGenerate: true,
     nametag: 'froreis',
   });
 
   if (created && generatedMnemonic) {
-    console.log('YENI CUZDAN OLUSTURULDU.');
+    console.log('YENI CUZDAN OLUSTURULDU!');
     console.log('Kurtarma ifadesi (KAYDET, KIMSEYLE PAYLASMA):');
     console.log(generatedMnemonic);
   }
 
-  console.log('Benim ID: @' + sphere.identity?.nametag);
+  console.log('Adres:', sphere.identity?.directAddress);
+  console.log('ID: @' + sphere.identity?.nametag);
 
-  const assets = await sphere.payments.getAssets();
-  console.log('Cuzdan bakiyesi:', assets);
+  try {
+    const assets = await sphere.payments.getAssets();
+    console.log('Cuzdan varliklari:', JSON.stringify(assets, null, 2));
+  } catch (e) {
+    console.log('Bakiye alinamadi:', e.message);
+  }
 
-  console.log('Bot hazir. Tip gondermek icin recipient ID ve amount belirtin.');
+  console.log('Bot hazir. Testnet2 uzerinde calisiyor.');
 }
 
 main().catch((err) => {
-  console.error('Hata olustu:', err);
+  console.error('Hata:', err.message);
+  process.exit(1);
 });
---
+++ serup.sh
@@ -0,0 +1,4 @@
+
+#!/bin/bash
+npm install @unicitylabs/sphere-sdk 
+npm install ws 
